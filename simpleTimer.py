7f4965ec7e6a8b1df47984d9158a18d6bd372154
Martin Klefas <martin.klefas@hotmail.com>
2025-09-13 23:00:10 +0100

    fixing tab/spaces with `black`

--- simpleTimer.py
+++ simpleTimer.py
@@ -1,5 +1,6 @@
 import time, io, csv
 
+
 class Timer:
     def __init__(self):
         self.start_times = {}
@@ -13,20 +14,24 @@ class Timer:
         self.event_stack.append(event_name)
         return self
 
-    def stop(self, event_name= None):
+    def stop(self, event_name=None):
         if event_name is None:
             if self.event_stack:
-                event_name = self.event_stack.pop()  # Pop the last event name from the stack
+                event_name = (
+                    self.event_stack.pop()
+                )  # Pop the last event name from the stack
             else:
                 raise ValueError("No event name provided and no event names in stack.")
         else:
             if event_name in self.event_stack:
-                self.event_stack.remove(event_name)  # Remove the specified event name from the stack
+                self.event_stack.remove(
+                    event_name
+                )  # Remove the specified event name from the stack
             else:
                 raise ValueError("Event name provided not found in stack.")
-        
-            
+
         self.stop_times[event_name] = time.time()
+
     def lap(self, new_event_name):
         # Stops the last started timer and starts a new one
         self.stop()
@@ -36,7 +41,7 @@ class Timer:
         # Stops the last started timer and starts a new one
         self.stop_looped()
         return self.start_looped(new_event_name)
-        
+
     def start_looped(self, event_name):
         if event_name not in self.loop_times:
             self.loop_times[event_name] = []
@@ -44,15 +49,16 @@ class Timer:
         self.event_stack.append(event_name)
         return self
 
-    def stop_looped(self, event_name= None):
+    def stop_looped(self, event_name=None):
         if not event_name:
             event_name = self.event_stack.pop()
         else:
             self.event_stack.remove(event_name)
-            
-        if event_name in self.loop_times:
-            self.loop_times[event_name][-1] = time.time() - self.loop_times[event_name][-1]
 
+        if event_name in self.loop_times:
+            self.loop_times[event_name][-1] = (
+                time.time() - self.loop_times[event_name][-1]
+            )
 
     def get_elapsed_time(self, event_name):
         start = self.start_times.get(event_name)
@@ -68,41 +74,47 @@ class Timer:
         for event_name in self.start_times:
             elapsed_time = self.get_elapsed_time(event_name)
             if elapsed_time is not None:
-                retval+=f'Elapsed time for {event_name}: {elapsed_time} seconds\n'
-                
+                retval += f"Elapsed time for {event_name}: {elapsed_time} seconds\n"
+
         for event_name in self.loop_times:
             times = self.loop_times.get(event_name)
             if times:
                 avg_time = sum(times) / len(times)
-                retval+=f'There were {len(times)} repeats of {event_name}, average duration was: {avg_time} seconds\n'
+                retval += f"There were {len(times)} repeats of {event_name}, average duration was: {avg_time} seconds\n"
         return retval
-        
+
     def reportCSV(self):
         listdictresults = []
         for event_name in self.start_times:
             elapsed_time = self.get_elapsed_time(event_name)
-            listdictresults.append({"event_name":event_name, "repeats":1, "Time":elapsed_time})
-                
+            listdictresults.append(
+                {"event_name": event_name, "repeats": 1, "Time": elapsed_time}
+            )
+
         for event_name in self.loop_times:
             times = self.loop_times.get(event_name)
             if times:
                 avg_time = sum(times) / len(times)
-                listdictresults.append({"event_name":event_name, "repeats":len(times), "Time":avg_time})
-                                       
+                listdictresults.append(
+                    {"event_name": event_name, "repeats": len(times), "Time": avg_time}
+                )
+
         output = io.StringIO()
-        fieldnames = ['Event', 'Repeats', '(avg) Duration']
+        fieldnames = ["Event", "Repeats", "(avg) Duration"]
         writer = csv.DictWriter(output, fieldnames=fieldnames)
         writer.writeheader()
-    
+
         for row in listdictresults:
-            writer.writerow({
-                                'Event': row['event_name'], 
-                                'Repeats': row['repeats'], 
-                                '(avg) Duration': row['Time']
-                            })
-            
+            writer.writerow(
+                {
+                    "Event": row["event_name"],
+                    "Repeats": row["repeats"],
+                    "(avg) Duration": row["Time"],
+                }
+            )
+
         csv_string = output.getvalue()
-    
+
         output.close()
 
         return csv_string
@@ -112,4 +124,3 @@ class Timer:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
-        
